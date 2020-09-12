bd50b2d872ef97999269185303546b7b78f81e5c
ChrisFSF <814218674@qq.com>
2020-09-12 14:10:42 -0700

    add ros ws for backup

---
+++ ROS_WS/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/pi/ROS_WS/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/pi/ROS_WS/devel;/opt/ros/kinetic".split(';') if "/home/pi/ROS_WS/devel;/opt/ros/kinetic" != "" else []
---
+++ ROS_WS/build/drone_project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/ROS_WS/src/drone_project
+# Build directory: /home/pi/ROS_WS/build/drone_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
