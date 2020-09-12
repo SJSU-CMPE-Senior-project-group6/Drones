47562b868ce5f45e126fedc993afa4357e51702f
ChrisFSF <814218674@qq.com>
2020-09-12 16:12:58 -0700

    rm all build and devel files

--- ROS_WS/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROS_WS/src
-# Build directory: /home/pi/ROS_WS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("drone_project")
--- ROS_WS/build/drone_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROS_WS/src/drone_project
-# Build directory: /home/pi/ROS_WS/build/drone_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
