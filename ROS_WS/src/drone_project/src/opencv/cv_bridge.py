f07c5b5f01f0499daf848a0b3b638b12d76c8985
lihanxiangli <lihanxiangli@gmail.com>
2020-10-06 23:15:08 -0700

    cv_bridge

---
+++ ROS_WS/src/drone_project/src/opencv/cv_bridge.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python2
+# -*- coding: UTF-8 -*-
+
+from cv_bridge import CvBridge
+bridge = CvBridge()
+cv_image = bridge.imgmsg_to_cv2(image_message, desired_encoding='passthrough')
+
+#------------------------------------------------------------
+
+import roslib
+roslib.load_manifest('my_package')
+import sys
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+	def __init__(self):
+		self.image_sub = rospy.Subscriber("topic_ros_image", Image, self.callback)
+		self.bridge = CvBridge()
+		self.image_pub = rospy.Publisher("topic_cv_image", Image, queue_size=1)
+		rospy.init_node('image_converter', anonymous=True)
+
+	def callback(self, image):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+			self.image_pub.publish(cv_image)
+		except CvBridgeError as e:
+			print(e)
+
+def main(args):
+	ic = image_converter()
+	rospy.spin()
+
+if __name__ == '__main__':
+	main(sys.argv)
