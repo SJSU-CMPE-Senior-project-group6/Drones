f6016615492d88d89495f84b73c34ed9d9ef87dc
ChrisFSF <814218674@qq.com>
2020-10-10 22:46:17 -0700

    update

---
+++ ROS_WS/src/drone_project/src/opencv/cv_command_publisher.py
@@ -0,0 +1,44 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# Create a CascadeClassifier Object
+face_cascade = cv2.CascadeClassifier('/home/ros/Desktop/Drones/ROS_WS/src/drone_project/src/opencv/model/lbpcascade_frontalface_improved.xml')
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array	
+	
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# Search the co-ordintes of the image
+	faces = face_cascade.detectMultiScale(gray,
+		scaleFactor = 1.1, minNeighbors = 4)
+	print(faces)
+	for x,y,w,h in faces:
+		image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 3)
+		cv2.putText(image, 'face', (x, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+		#print(x," ",y," ",w," ",h)	
+
+
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
