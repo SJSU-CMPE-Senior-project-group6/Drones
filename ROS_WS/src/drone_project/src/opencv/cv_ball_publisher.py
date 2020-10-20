0a50ff0fcfc4e745ab3839a89bc02f9f7d215fcd
ChrisFSF <814218674@qq.com>
2020-10-20 00:10:41 -0700

    modify cv ball publisher

--- ROS_WS/src/drone_project/src/opencv/cv_ball_publisher.py
+++ ROS_WS/src/drone_project/src/opencv/cv_ball_publisher.py
@@ -54,7 +54,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	# show the frame
 	cv2.imshow("Frame", image)
-	#cv2.imshow("mask", red_mask)
+	cv2.imshow("mask", red_mask)
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
