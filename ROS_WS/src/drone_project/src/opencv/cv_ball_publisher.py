4530636cef00a89b050a45c32204a6643a835656
ChrisFSF <814218674@qq.com>
2020-10-19 23:32:25 -0700

    update

--- ROS_WS/src/drone_project/src/opencv/cv_ball_publisher.py
+++ ROS_WS/src/drone_project/src/opencv/cv_ball_publisher.py
@@ -6,12 +6,10 @@ from sensor_msgs.msg import Image
 import rospy
 import time
 import cv2
-
+import numpy as np
 #ROS
 rospy.init_node('opencv_publisher')
 pub = rospy.Publisher('/cv_info',String,queue_size=2)
-# Create a CascadeClassifier Object
-face_cascade = cv2.CascadeClassifier('/home/ros/Desktop/Drones/ROS_WS/src/drone_project/src/opencv/model/lbpcascade_frontalface_improved.xml')
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -26,29 +24,42 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# and occupied/unoccupied text
 	image = frame.array	
 	
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	cols = len(image[0])
+	rows = len(image)	
+	x_medium = int(cols/2)
+	center = int(cols/2)
+	
+	#blur = cv2.GaussianBlur(image, (5,5), 0)
+	hsv_frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-	# Search the co-ordinates of the image
-	faces = face_cascade.detectMultiScale(gray,
-		scaleFactor = 1.1, minNeighbors = 4)
+	#red color 
+	lower_red = np.array([161, 155, 84])
+	upper_red = np.array([179, 255, 255])
+	red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+	_, contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+	
 	str_send = ""
-	for x,y,w,h in faces:
+
+	for cnt in contours:
+		(x, y, w, h) = cv2.boundingRect(cnt)
+
 		str_send += str(x)+","+str(y)+","+str(w)+","+str(h)+"," 		
 		print(str_send)
 		pub.publish(str_send)
-		
-		#image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 3)
-		#cv2.putText(image, 'face', (x, y),
-			#cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)        
-		#print(x," ",y," ",w," ",h)	
+
+		x_medium = int((x + x + w) / 2 )
+		break
+	cv2.line(image, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
 
 	# show the frame
-	#cv2.imshow("Frame", image)
-	#key = cv2.waitKey(1) & 0xFF
+	cv2.imshow("Frame", image)
+	#cv2.imshow("mask", red_mask)
+	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
 
 
 	#if the `q` key was pressed, break from the loop
-	#if key == ord("q"):
-	#	break
+	if key == ord("q"):
+		break
--- ROS_WS/src/drone_project/src/opencv/cv_command_publisher.py
+++ ROS_WS/src/drone_project/src/opencv/cv_command_publisher.py
@@ -1,44 +1,36 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
+#! /usr/bin/env python2
+# -*- coding: UTF-8 -*-
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
-# Create a CascadeClassifier Object
-face_cascade = cv2.CascadeClassifier('/home/ros/Desktop/Drones/ROS_WS/src/drone_project/src/opencv/model/lbpcascade_frontalface_improved.xml')
+class image_info:
+	def __init__(self):
+		rospy.init_node('command_publisher')
+		self.image_sub = rospy.Subscriber("/cv_info", String, self.callback)
+		self.image_pub = rospy.Publisher("/cv_command", Twist, queue_size=2)
+		self.value = [0,0,0,0] #x y w h
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array	
-	
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		rospy.spin()
 
-	# Search the co-ordintes of the image
-	faces = face_cascade.detectMultiScale(gray,
-		scaleFactor = 1.1, minNeighbors = 4)
-	print(faces)
-	for x,y,w,h in faces:
-		image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 3)
-		cv2.putText(image, 'face', (x, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
-		#print(x," ",y," ",w," ",h)	
+	def callback(self, msgs):
+		command = msgs.data
+		comma_counter = 0
+		i = 0
+		temp = ""
 
+		while True:
+			if command[i] == ",":
+				self.value[comma_counter] = int(temp)
+				temp = ""	
+				comma_counter += 1
+			else:	
+				temp += command[i]
+			i += 1
+			if comma_counter == 4:
+				break
+		
+		print(self.value)
 
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+if __name__ == "__main__":
+	image_cv = image_info()
