21acf692fcde83dd6d5d66f48a331a065985d496
ChrisFSF <814218674@qq.com>
2020-10-27 16:19:59 -0700

    update

--- ROS_WS/src/drone_project/src/accel_ctl.py
+++ ROS_WS/src/drone_project/src/accel_ctl.py
@@ -168,13 +168,13 @@ class Accel_Publisher(object):
                 yaw_error = self.target_yaw - self.channel[3]
                 pitch_error = self.target_pitch - self.channel[1]
                 print("pitch:",self.target_pitch, "yaw:",self.target_yaw)
-                self.throttle_change_rate = self.pid_control(throttle_error, 4, 1.5, 0.1)
+                self.throttle_change_rate = self.pid_control(throttle_error, 3, 1.5, 0.1)
                 self.yaw_change_rate = self.pid_control(yaw_error, 2, 0.001, 5)
                 self.pitch_change_rate = self.pid_control(pitch_error, 4, 0.001, 5)
                 # self.roll_change_rate = self.pid_control(current_error, 4, 0.001, 5)
 
-                print("throttle rate: ",round(self.throttle_change_rate,2), "error: ",round(throttle_error,2))
-                #print("yaw rate: ",round(self.throttle_change_rate,2), "error: ",round(yaw_error,2))
+                #print("throttle rate: ",round(self.throttle_change_rate,2), "error: ",round(throttle_error,2))
+                print("yaw rate: ",round(self.yaw_change_rate,2), "error: ",round(yaw_error,2))
                 #print("pitch rate: ",round(self.throttle_change_rate,2), "error: ",round(pitch_error,2))
 
                 if self.launch_status == True:
